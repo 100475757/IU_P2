06bbd6d5aab2c28ca68d679ee17f365982d74b9e
100475757 <100475757@alumnos.uc3m.es>
2024-10-28 19:49:49 +0100

    "cambios en el login odal y register modal"

--- main.js
+++ main.js
@@ -29,67 +29,106 @@ function calcularTiempoRestante() {
 }
 
 const interval = setInterval(calcularTiempoRestante, 1000);
-calcularTiempoRestante();
 
-
-// Registro de usuarios
-
+// Modal register
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('btn-open-register').addEventListener('click', function() {
+        registerForm.reset();
+        document.getElementById('childrenDetails').innerHTML = '';
         document.getElementById('modal-register').showModal();
     });
+    document.getElementById('clearFields').addEventListener('click', function() {
+        if (confirm('¿Estás seguro de que deseas limpiar todos los campos?')) {
+            document.getElementById('registerForm').reset();
+            document.getElementById('childrenDetails').innerHTML = '';
+        }
+    });
     document.getElementById('btn-close-register').addEventListener('click', function() {
-        if (confirm('¿Estás seguro de que quieres cerrar la ventana?')) {
+        if (confirm('¿Estás seguro de que deseas cancelar el registro?')) {
             document.getElementById('modal-register').close();
+            document.getElementById('registerForm').reset();
+            document.getElementById('childrenDetails').innerHTML = '';
         }
     });
 
-    document.getElementById('register-form').addEventListener('reset', function(event) {
-        if (!confirm('¿Estás seguro de que quieres limpiar todos los campos?')) {
-            event.preventDefault();
+    // Generación dinámica de campos de hijos
+    const childrenInput = document.getElementById("children");
+    const childrenDetails = document.getElementById("childrenDetails");
+
+    childrenInput.addEventListener("input", function() {
+        const numChildren = parseInt(childrenInput.value);
+        childrenDetails.innerHTML = ""; // Limpiar campos previos
+
+        if (numChildren > 0) {
+            for (let i = 1; i <= numChildren; i++) {
+                const childDiv = document.createElement("div");
+                childDiv.classList.add("child-info");
+
+                const nameLabel = document.createElement("label");
+                nameLabel.textContent = `Nombre del hijo/hija ${i}`;
+                const nameInput = document.createElement("input");
+                nameInput.type = "text";
+                nameInput.id = `childName${i}`;
+                nameInput.name = `childName${i}`;
+                nameInput.minLength = 3;
+                nameInput.required = true;
+
+                const ageLabel = document.createElement("label");
+                ageLabel.textContent = `Edad del hijo/hija ${i}`;
+                const ageInput = document.createElement("input");
+                ageInput.type = "number";
+                ageInput.id = `childAge${i}`;
+                ageInput.name = `childAge${i}`;
+                ageInput.min = 1;
+                ageInput.required = true;
+
+                const toysLabel = document.createElement("label");
+                toysLabel.textContent = `Juguetes favoritos del hijo/hija ${i}`;
+                const toysInput = document.createElement("input");
+                toysInput.type = "text";
+                toysInput.id = `childToys${i}`;
+                toysInput.name = `childToys${i}`;
+                toysInput.required = true;
+
+                childDiv.appendChild(nameLabel);
+                childDiv.appendChild(nameInput);
+                childDiv.appendChild(ageLabel);
+                childDiv.appendChild(ageInput);
+                childDiv.appendChild(toysLabel);
+                childDiv.appendChild(toysInput);
+
+                childrenDetails.appendChild(childDiv);
+            }
         }
     });
 
-    document.getElementById('children').addEventListener('input', function() {
-        const childrenCount = parseInt(this.value, 10);
-        const childrenDetails = document.getElementById('children-details');
-        childrenDetails.innerHTML = '';
-
-        for (let i = 0; i < childrenCount; i++) {
-            const childDiv = document.createElement('div');
-            childDiv.classList.add('form-group-reg');
-            childDiv.innerHTML = `
-                <label for="child-name-${i}">Nombre del hijo/hija ${i + 1} *</label>
-                <input type="text" id="child-name-${i}" name="child-name-${i}" required minlength="3" placeholder="Introduce el nombre del hijo/hija ${i + 1}">
-                <label for="child-age-${i}">Edad del hijo/hija ${i + 1} *</label>
-                <input type="number" id="child-age-${i}" name="child-age-${i}" required min="0" placeholder="Introduce la edad del hijo/hija ${i + 1}">
-                <label for="child-toys-${i}">Juguetes favoritos del hijo/hija ${i + 1}</label>
-                <input type="text" id="child-toys-${i}" name="child-toys-${i}" placeholder="Introduce los juguetes favoritos del hijo/hija ${i + 1}">
-            `;
-            childrenDetails.appendChild(childDiv);
-        }
-    });
-
-    document.getElementById('register-form').addEventListener('submit', function(event) {
+    // Registro y validación
+    document.getElementById('registerForm').addEventListener('submit', function(event) {
         event.preventDefault();
-
+        
         const username = document.getElementById('username').value;
         const password = document.getElementById('password').value;
-        const confirmPassword = document.getElementById('confirm-password').value;
         const email = document.getElementById('email').value;
-        const city = document.getElementById('city').value;
-        const country = document.getElementById('country').value;
-        const gender = document.getElementById('gender').value;
-        const children = parseInt(document.getElementById('children').value, 10);
+        const city = document.getElementById('city-registro').value;
+        const country = document.getElementById('country-registro').value;
+        const gender = document.getElementById('gender-registro').value;
+        const childrenCount = parseInt(document.getElementById('children').value, 10);
+
+        // Validar nombre de usuario
+        if (username.length < 3) {
+            alert('El nombre de usuario debe tener al menos 3 caracteres.');
+            return;
+        }
 
         // Validar contraseña
-        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d{2,})(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{12,}$/;
+        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d.*\d)(?=.*\W)(?=.{12,})/;
         if (!passwordRegex.test(password)) {
-            alert('La contraseña debe tener al menos 12 caracteres, con mínimo 2 números, 1 carácter especial, 1 letra mayúscula y 1 letra minúscula.');
+            alert('La contraseña debe tener 12 caracteres de longitud, con mínimo 2 números, 1 carácter especial, 1 letra mayúscula y 1 letra minúscula.');
             return;
         }
 
         // Validar confirmación de contraseña
+        const confirmPassword = document.getElementById('confirmPassword').value;
         if (password !== confirmPassword) {
             alert('Las contraseñas no coinciden.');
             return;
@@ -102,30 +141,109 @@ document.addEventListener('DOMContentLoaded', function() {
             return;
         }
 
-        const user = {
-            username: username,
-            password: password,
-            email: email,
-            city: city,
-            country: country,
-            gender: gender,
+        // Validar ciudad
+        if (city.length < 3) {
+            alert('La ciudad debe tener al menos 3 caracteres.');
+            return;
+        }
+
+        // Validar país
+        if (country.length < 3) {
+            alert('El país debe tener al menos 3 caracteres.');
+            return;
+        }
+
+        // Validar detalles de hijos
+        for (let i = 1; i <= childrenCount; i++) {
+            const childName = document.getElementById(`childName${i}`).value;
+            const childAge = document.getElementById(`childAge${i}`).value;
+            const childToys = document.getElementById(`childToys${i}`).value;
+
+            if (childName.length < 3) {
+                alert(`El nombre del hijo/hija ${i} debe tener al menos 3 caracteres.`);
+                return;
+            }
+
+            if (childAge <= 0) {
+                alert(`La edad del hijo/hija ${i} debe ser un número positivo.`);
+                return;
+            }
+
+            if (childToys.length < 3) {
+                alert(`Los juguetes favoritos del hijo/hija ${i} deben tener al menos 3 caracteres.`);
+                return;
+            }
+        }
+
+        // Obtener usuarios almacenados en Local Storage o inicializar un array vacío
+        const users = JSON.parse(localStorage.getItem('users')) || [];
+
+        // Crear objeto del nuevo usuario
+        const userData = {
+            username,
+            password,
+            email,
+            city,
+            country,
+            gender,
             children: []
         };
 
-        for (let i = 0; i < children; i++) {
-            const childName = document.getElementById(`child-name-${i}`).value;
-            const childAge = document.getElementById(`child-age-${i}`).value;
-            const childToys = document.getElementById(`child-toys-${i}`).value;
-
-            user.children.push({
-                name: childName,
-                age: childAge,
-                toys: childToys
+        for (let i = 1; i <= childrenCount; i++) {
+            userData.children.push({
+                name: document.getElementById(`childName${i}`).value,
+                age: document.getElementById(`childAge${i}`).value,
+                toys: document.getElementById(`childToys${i}`).value
             });
         }
 
-        localStorage.setItem(username, JSON.stringify(user));
-        alert('Usuario registrado con éxito');
+        // Añadir nuevo usuario al array de usuarios
+        users.push(userData);
+
+        // Guardar el array de usuarios actualizado en Local Storage
+        localStorage.setItem('users', JSON.stringify(users));
+        alert('Registro exitoso');
         document.getElementById('modal-register').close();
     });
 });
+
+// Modal Login
+    
+document.addEventListener('DOMContentLoaded', function() {
+    // Botón para abrir el formulario de inicio de sesión
+    document.getElementById('btn-open-login').addEventListener('click', function() {
+        document.getElementById('loginForm').reset();
+        document.getElementById('modal-login').showModal();
+    });
+
+    // Botón para cerrar el formulario de inicio de sesión
+    document.getElementById('btn-close-login').addEventListener('click', function() {
+        document.getElementById('modal-login').close();
+    });
+
+    // Enviar formulario de inicio de sesión
+    document.getElementById('loginForm').addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const loginUsername = document.getElementById('login-username').value;
+        const loginPassword = document.getElementById('login-password').value;
+
+        // Obtener los usuarios almacenados en Local Storage
+        const users = JSON.parse(localStorage.getItem('users')) || [];
+        const user = users.find(u => u.username === loginUsername && u.password === loginPassword);
+
+        if (user) {
+            alert('Inicio de sesión exitoso');
+            document.getElementById('modal-login').close();
+            
+        } else {
+            alert('Usuario o contraseña incorrectos');
+        }
+    });
+});
+
+    
+
+
+        
+
